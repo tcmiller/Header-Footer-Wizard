69ebe749b9a3e97f3554ca6520b5d0caf5a85cd0
Chris Heiland <cheiland@uw.edu>
2009-10-07 16:58:27 +0000

    Removed old versions of sql

--- template_wizard/template.20090903.sql
+++
@@ -1,5 +0,0 @@
-BEGIN TRANSACTION;
-CREATE TABLE account (id integer primary key auto_increment, requestor varchar(50), owner varchar(50), secondary varchar(50), email varchar(150), site_url varchar(150), active binary NOT NULL, code_pref enum('copy-paste','include','both'), created_date datetime, modified_date datetime, last_accessed datetime);
-CREATE TABLE header (id integer primary key auto_increment, kitchen_sink binary, blockw binary, wordmark binary, color enum('purple','gold'), search ('basic','super-inline','super-tab'), created_date datetime, last_modified datetime, account_id integer);
-CREATE TABLE footer (id integer primary key autoincrement, blockw binary, wordmark binary, patch enum('purple','gold','0'), created_date datetime, last_modified datetime, account_id integer);
-COMMIT;
--- template_wizard/template.sql
+++ template_wizard/template.sql
@@ -1,6 +1,5 @@
 BEGIN TRANSACTION;
-DELETE FROM sqlite_sequence;
-CREATE TABLE contact (netid varchar(50) primary key, email varchar(150), active binary, created_date datetime, modified_date datetime, last_accessed datetime, backup varchar(150));
-CREATE TABLE footer (id integer primary key autoincrement, blockw binary, wordmark binary, color varchar(50), patch binary, created_date datetime, modified_date datetime, last_accessed datetime, netid varchar(50));
-CREATE TABLE header (id integer primary key autoincrement, blockw binary, wordmark binary, color varchar(50), search varchar(50), created_date datetime, modified_date datetime, last_accessed datetime, netid varchar(50));
+CREATE TABLE account (id integer primary key auto_increment, requestor varchar(50), owner varchar(50), secondary varchar(50), email varchar(150), site_url varchar(150), active binary NOT NULL, code_pref enum('copy-paste','include','both'), created_date datetime, modified_date datetime, last_accessed datetime);
+CREATE TABLE header (id integer primary key auto_increment, kitchen_sink binary, blockw binary, wordmark binary, color enum('purple','gold'), search ('basic','super-inline','super-tab'), created_date datetime, last_modified datetime, account_id integer);
+CREATE TABLE footer (id integer primary key autoincrement, blockw binary, wordmark binary, patch enum('purple','gold','0'), created_date datetime, last_modified datetime, account_id integer);
 COMMIT;
